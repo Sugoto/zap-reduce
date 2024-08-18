baaa0bd388adaa14dc8bc577379a3e5815eaabcf
Sugoto <sugotobasu1@gmail.com>
2024-08-18 14:41:48 +0530

    Add Reduce function to aggregate word counts

---
+++ mapreduce/reduce.go
@@ -0,0 +1,12 @@
+package mapreduce
+
+// Reduce function aggregates word counts.
+func Reduce(intermediate []map[string]int) map[string]int {
+	finalCounts := make(map[string]int)
+	for _, counts := range intermediate {
+		for word, count := range counts {
+			finalCounts[word] += count
+		}
+	}
+	return finalCounts
+}
