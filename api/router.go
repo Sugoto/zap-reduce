2296f501c9b291ecd39495c7301a7a2d890515ab
Sugoto <sugotobasu1@gmail.com>
2024-08-18 14:43:40 +0530

    Remove unused comment in router.go

--- api/router.go
+++ api/router.go
@@ -4,7 +4,6 @@ import (
 	"github.com/gorilla/mux"
 )
 
-// NewRouter creates a new router for the API.
 func NewRouter() *mux.Router {
 	router := mux.NewRouter()
 	router.HandleFunc("/process", ProcessText).Methods("POST")
