b14b53780a5143b19bbb3c7376d080c7a72fbaec
Sugoto Basu <60142374+Sugoto@users.noreply.github.com>
2024-08-20 20:22:53 +0530

    Update worker.go

--- mapreduce/worker.go
+++ mapreduce/worker.go
@@ -1,7 +1,6 @@
 package mapreduce
 
 func Execute(text string) map[string]int {
-	// Split text into chunks (for simplicity, we'll process it as a single chunk)
 	chunks := []string{text}
 
 	// Map phase
